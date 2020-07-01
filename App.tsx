eebdf6b86853e14686890b39cf7e56fa64168370
Lemann89 <stevedoh@mail.ru>
2020-07-02 01:14:19 +0300

    renderImages

--- App.tsx
+++ App.tsx
@@ -1,49 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { PureComponent } from 'react';
-import { StyleSheet, Text, View, ScrollView, SafeAreaView, Button, FlatList, TouchableOpacity, Image } from 'react-native';
-import { HttpService } from './src/services/http.service';
-import { IPhotos } from "./src/models/photos";
-import PhotoCard from './src/components/PhotoCard';
+import {StatusBar} from "expo-status-bar";
+import React, {PureComponent} from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  SafeAreaView,
+  Button,
+  FlatList,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import {HttpService} from "./src/services/http.service";
+import {IPhotos} from "./src/models/photos";
+import PhotoCard from "./src/components/PhotoCard";
 
 export default class App extends PureComponent {
-
   state = {
-    imageList: [],
-  }
+    imageList: []
+  };
 
   http = new HttpService();
 
   componentDidMount() {
-    this.http.getAllPhotos().then(photos => this.setState({imageList: photos}));
+    this.http
+      .getAllPhotos()
+      .then((photos) => this.setState({imageList: photos}));
   }
 
-  renderPhotoCard(photo: IPhotos) {
-    return <PhotoCard photo={photo}/>
-}
-
   render() {
-    const {imageList} = this.state
-
-    return <SafeAreaView style={styles.container}>
-              <View>
-                <FlatList 
-                  data = {imageList}
-                  renderItem = {(item:IPhotos) => this.renderPhotoCard(item)}
-                  keyExtractor = {(item:IPhotos) => item.id.toString()}
-                />
-                <StatusBar style="auto" />
-              </View>
-          </SafeAreaView>
+    const {imageList} = this.state;
+    return (
+      <SafeAreaView style={styles.container}>
+        <View>
+          <FlatList
+            data={imageList}
+            renderItem={({item}) => <PhotoCard photo={item}/>}
+            keyExtractor={(item: IPhotos) => item.id.toString()}
+          />
+          <StatusBar style="auto" />
+        </View>
+      </SafeAreaView>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
-    backgroundColor: '#fff',
+    display: "flex",
+    backgroundColor: "#fff",
     marginTop: 30,
   },
   blockText: {
     fontSize: 20,
-  }
+  },
 });
--- src/components/PhotoCard.tsx
+++ src/components/PhotoCard.tsx
@@ -1,39 +1,34 @@
-import React, { PureComponent } from 'react';
-import { StyleSheet, View, TouchableOpacity, Image } from 'react-native';
-import { IPhotos } from "../models/photos";
+import React, {PureComponent} from "react";
+import {StyleSheet, View, TouchableOpacity, Image} from "react-native";
+import {IPhotos} from "../models/photos";
 
 interface Props {
-    photo: {
-        item: IPhotos
-    }
+  photo: IPhotos
 }
 
 export default class PhotoCard extends PureComponent<Props> {
-    render() {
-        const { photo } = this.props
-        return (
-            <TouchableOpacity style={{backgroundColor: 'transparent'}}>
-                <View style={styles.flexGrid}>
-                    <Image style={styles.block} source={{uri: `${photo.item.url}`}} />
-                    {console.log(this.props)}
-                    
-                </View>
-            </TouchableOpacity>
-        )
-    }
+  render() {
+    const {photo} = this.props;
+    return (
+      <TouchableOpacity style={{backgroundColor: "transparent"}}>
+        <View style={styles.flexGrid}>
+          <Image style={styles.block} source={{uri: `${photo.url}`}} />
+        </View>
+      </TouchableOpacity>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    flexGrid: {
-      flexDirection: 'column',
-      flexWrap: 'wrap'
-    },
-    block: {
-      margin: 5,
-      width: '97%',
-      height: 100,
-      justifyContent: 'center',
-      alignItems: 'center',
-    }
-  });
-  
+  flexGrid: {
+    flexDirection: "column",
+    flexWrap: "wrap",
+  },
+  block: {
+    margin: 5,
+    width: "97%",
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
